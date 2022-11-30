c1534095df5aff1d3a0bccc6a37e2fe4606c330d
Kate4ka007 <shlemena@mail.ru>
2022-12-01 01:08:22 +0300

    feat: add solution for tasks compress, decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from "stream";
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.',
+    import.meta.url));
+
+const FILE_NAME = 'fileToCompress.txt';
+const FILE_ZIP = 'archive.gz';
+const PATH_FILE = path.join(__dirname, 'files', FILE_NAME);
+const PATH_FILE_ZIP = path.join(__dirname, 'files', FILE_ZIP);
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createGunzip();
+    const readStream = createReadStream(PATH_FILE_ZIP);
+    const writeStream = createWriteStream(PATH_FILE);
+
+    pipeline(readStream, unzip, writeStream, (error) => {
+        if (error) throw new Error('Something went wrong');
+    });
 };
 
 await decompress();
