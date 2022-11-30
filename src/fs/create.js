0cc2b6799a9898454d7dd450cb8875222b912aa0
Kate4ka007 <shlemena@mail.ru>
2022-11-30 15:33:39 +0300

    feat: add solution for task create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import * as fs from "fs";
+
+const PATH = "./files/fresh.txt";
+const MESSAGE = 'I am fresh and young';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(PATH, MESSAGE, { flag: 'wx' }, (error) => {
+        if (error) throw new Error(ERROR_MESSAGE);
+    });
 };
 
-await create();
+create();
